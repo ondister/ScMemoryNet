fd0a799e2104070fb89d27cb165844502ad16702
Andrey Bychko <courage.andrey@gmail.com>
2016-06-24 21:09:53 +0300

    Выполнен code cleanup в файле ExNetPullEnty/ExNetPullenty.cs.

--- ExNetPullEnty/ExNetPullenty.cs
+++ ExNetPullEnty/ExNetPullenty.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using ScEngineNet;
+﻿using ScEngineNet;
 using ScEngineNet.ExtensionsNet;
-using ScEngineNet.NativeElements;
 using ScEngineNet.SafeElements;
 
 namespace ExNetPullEnty
 {
-    public class ExNetPullenty:IScExtensionNet
+    public class ExNetPullenty : IScExtensionNet
     {
-
         #region Члены IScExtensionNet
+
         private ScMemoryContext context;
-        PullentiEngine pEngine;
+        private PullentiEngine pullentiEngine;
+
         public string NetExtensionName
         {
             get { return "Расширение .Net Pullenti"; }
@@ -25,39 +20,20 @@ namespace ExNetPullEnty
         {
             get { return "Расширение .Net для преобразования содержимого ссылок в семантические конструкции"; }
         }
-        
+
         public ScResult Initialize()
         {
            context = new ScMemoryContext(ScAccessLevels.MaxLevel);
-
-           
-
-
-           //some code 
-         pEngine = new PullentiEngine(this.context);
-     
-            return ScResult.SC_RESULT_OK;
+           pullentiEngine = new PullentiEngine(this.context);
+           return ScResult.SC_RESULT_OK;
         }
 
         public ScResult ShutDown()
         {
-            //some code
-            pEngine.DeleteEvents();
-            //
+            pullentiEngine.DeleteEvents();
             context.Delete();
-                     
-         
             return ScResult.SC_RESULT_OK;
         }
-
-        #endregion
-
-
-
-        #region Члены IScExtensionNet
-
-
-       
 
         #endregion
     }
