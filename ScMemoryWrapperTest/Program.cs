cc45f1ff1cd808622ed2657dd54f50c7ea9318f9
Andrey Bychko <courage.andrey@gmail.com>
2016-06-24 21:06:55 +0300

    Выполнен code cleanup в файле ScMemoryWrapperTest/Program.cs.

--- ScMemoryWrapperTest/Program.cs
+++ ScMemoryWrapperTest/Program.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.Diagnostics;
-using ScEngineNet;
+
 using ScEngineNet.SafeElements;
-using ScEngineNet.NativeElements;
-using System.Collections.Generic;
 using ScMemoryNet;
-using System.Runtime.InteropServices;
+
 namespace Demo
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-
-
-            string configFile = @"d:\OSTIS\sc-machine-iot\bin\sc-memory.ini";
-            string repoPath = @"d:\OSTIS\sc-machine-iot\bin\repo";
-            string extensionPath = @"d:\OSTIS\sc-machine-iot\bin\extensions";
-            string netExtensionPath = @"d:\OSTIS\sc-machine-iot\bin\netextensions";
-
-
+            const string configFile = @"d:\OSTIS\sc-machine-iot\bin\sc-memory.ini";
+            const string repoPath = @"d:\OSTIS\sc-machine-iot\bin\repo";
+            const string extensionPath = @"d:\OSTIS\sc-machine-iot\bin\extensions";
+            const string netExtensionPath = @"d:\OSTIS\sc-machine-iot\bin\netextensions";
 
             ScMemory.Initialize(true, configFile, repoPath, extensionPath, netExtensionPath);
 
@@ -30,23 +22,12 @@ namespace Demo
             {
                 var link = ScMemory.BaseContext.CreateLink(new ScLinkContent("Этот текст будет обработан расширением"));
                 var link2 = ScMemory.BaseContext.CreateLink(new ScLinkContent(" И этот текст будет обработан расширением"));
-              
-
                 ScMemory.BaseContext.CreateArc(pNode, link, ElementType.AccessArc_a);
                 ScMemory.BaseContext.CreateArc(pNode, link2, ElementType.AccessArc_a);
-              
             }
 
             Console.ReadKey();
-
-
             Console.WriteLine(ScMemory.ShutDown(true));
-           
         }
-
-      
-
-
-
     }
 }
