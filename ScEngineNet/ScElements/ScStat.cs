45404a603bef76239d56e09868338f64294ac1eb
Andrey Bychko <bublic-xiii@list.ru>
2018-12-05 22:04:04 +0300

    Вставлено несколько пустых строк в класс ScStat для повышения читабельности кода.

--- ScEngineNet/ScElements/ScStat.cs
+++ ScEngineNet/ScElements/ScStat.cs
@@ -9,18 +9,22 @@
         /// The node count
         /// </summary>
         public uint NodeCount;
+
         /// <summary>
         /// The arc count
         /// </summary>
         public uint ArcCount;
+
         /// <summary>
         /// The link count
         /// </summary>
         public uint LinkCount;
+
         /// <summary>
         /// The empty count
         /// </summary>
         public uint EmptyCount;
+
         /// <summary>
         /// The segments count
         /// </summary>
